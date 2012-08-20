9ef5e8a99e993a5e858316bf3e2cbc616875bfbe
Kenneth Nordahl <kenneth@nordahl.me>
2012-08-20 08:33:36 +0200

    Initial commit
    Signed-off-by: Kenneth Nordahl <kenneth@nordahl.me>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HiofDev::Application.config.secret_token = '23be091d76d5438de460f0de905eebbb373b85a74c1c56eff4755da52403b3809aa586e72ada51566e509de58c39ebefd2c1d1106a68cc5b5abaf481993c9047'
